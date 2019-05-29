57501545bf94179ac7ef08ffa65aa2713c8ad60a
Ihor Bespalyy <i.bespalyy@newlinetechnologies.net>
2019-05-29 14:20:32 +0300

    Updates

---
+++ precache-manifest.c2b394ba301034e3787974edae3fba3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ed467a271c46ee1c3937bccda56008",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a3deca09495b6ca33997",
+    "url": "/test/static/css/main.d0347ada.chunk.css"
+  },
+  {
+    "revision": "6afb4d93dc025bca6d86",
+    "url": "/test/static/js/2.74286c36.chunk.js"
+  },
+  {
+    "revision": "a3deca09495b6ca33997",
+    "url": "/test/static/js/main.29b3955c.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
